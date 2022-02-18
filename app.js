451d9c12558accb5d0e15def21ffb15ba767eeba
Wendel Cutrim <wendel@digitalhouse.com>
2022-02-18 10:59:10 -0300

    Realizado a parte 4 do exercício

--- app.js
+++ app.js
@@ -1,1 +1,5 @@
 let paises = [];
+
+paises.push('Brasil', 'Argentina', 'Colombia','Peru','Chile', 'Equador', 'Bosnia', 'Venezuela', 'Bolívia', 'Uruguai', 'Guiana', 'Suriname', 'Paraguai', 'Guiana Francesa');
+
+console.table(paises);
