37d818bb932a8c7c6e522885806738c25e1432b1
Wendel Cutrim <wendel@digitalhouse.com>
2022-02-18 11:20:48 -0300

    Realizado a parte 5 do exercício

--- app.js
+++ app.js
@@ -2,4 +2,17 @@ let paises = [];
 
 paises.push('Brasil', 'Argentina', 'Colombia','Peru','Chile', 'Equador', 'Bosnia', 'Venezuela', 'Bolívia', 'Uruguai', 'Guiana', 'Suriname', 'Paraguai', 'Guiana Francesa');
 
-console.table(paises);
+//Forma tradicional
+for (let i = 0; i <= paises.length; i++){
+    console.log(paises[i]);
+}
+
+//Itera 
+paises.forEach((pais) => {
+    return console.log(pais)
+})
+
+//Iterar sobre array
+for (let pais of paises){
+    console.log(pais)
+}
